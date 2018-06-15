1293e41c56930d04e77fb6ff007d3ce757839128
Rachel Krasnick <rachkras@umich.edu>
2018-06-15 22:17:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function sayHiToGrandma(string) {
  if (string.toLowerCase() === string)
   return "I can't hear you!"
   
- if (string.toUpperCase() === string)
+ else if (string.toUpperCase() === string)
   return "YES INDEED!"
+  
+ else if (string === "I love you, Grandma.")
+ return "I love you,too."
 }
