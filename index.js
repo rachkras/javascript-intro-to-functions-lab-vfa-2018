de5331791b7db0c92d69880bd05fe318fc825e01
Rachel Krasnick <rachkras@umich.edu>
2018-06-15 22:08:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   string.toLowerCase() === string
   return "I can't hear you!"
-}
-
-function sayHiToGrandma(string) {
+  
   string.toUpperCase() === string
   return "YES INDEED!"
 }
